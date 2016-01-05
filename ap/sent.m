33e51d77a73bd28759176c6c06936372f061f892
Yogesh Bodhe <bodheyogesh1991@gmail.com>
2016-01-05 13:39:21 -0500

    Delete sent.m

--- ap/sent.m
+++
@@ -1,6 +0,0 @@
-a = readfis('sentence');
-a = addvar(a,'input','noun',[0 30]);
-a = addmf(a,'input',1,'Low','gaussmf',[0 10 10]);
-a = addmf(a,'input',1,'Medium','gaussmf',[10 15 20]);
-a = addmf(a,'input',1,'High','gaussmf',[20 25 30]);
-plotmf(a,'input',1)
